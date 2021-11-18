25324b36e01f97b273869e7f0bebb9758d0442c1
shiwensheng <shiwensheng@telecomyt.com.cn>
2021-11-18 17:04:04 +0800

    commit

--- app/src/main/java/com/mylibrary/swslibrary/kotlin/AdvanceMethod.kt
+++ app/src/main/java/com/mylibrary/swslibrary/kotlin/AdvanceMethod.kt
@@ -8,11 +8,18 @@ import com.mylibrary.swslibrary.utils.SLog
  *@msg
  **/
 fun main() {
-
+    //创建一个元素为1,2,3的Int类型的List集合
     val mList = listOf(1, 2, 3)
-    val result = mList.sum { SLog.print("调用高阶函数-函数作为参数 遍历集合元素 it-value =  $it") }
+    //调用自定义的高阶函数sum
+    val result = mList.sum {
+        //参数只有一个的时候 就可以使用it代替
+        SLog.print("调用高阶函数-函数作为参数 遍历集合元素 it-value =  $it")
+    }
     SLog.print("调用高阶函数-函数作为参数 输出返回值 result  =  $result")
 
+
+    //调用自定义的高阶函数toIntSum
+    // 因为toIntSum的返回值也是一个函数 可以使用toIntSum()(3)这个方式来调用他返回的函数 并传递一个Int类型的值：3
     val result2 = mList.toIntSum()(3)
     SLog.print("函数作为返回值 调用函数返回的函数 返回的value = $result2")
 
@@ -22,8 +29,25 @@ fun main() {
     //解构声明
     test2()
 
+    //调用匿名方法
+    val fun1Result = fun1(1, 2)
+    SLog.print("调用匿名方法 fun1 value = $fun1Result")
+    val fun2Result = fun2(1, 2)
+    SLog.print("调用匿名方法 fun2 value = $fun2Result")
+
     //方法字面值
     literal()
+
+    //定义一个变量mResult 他接受一个参数类型为Int 返回值为boolean类型的函数
+    var mResult: ((Int) -> Boolean)? = null
+    //使用正常方式为mResult赋值函数
+    //mResult = { p -> p > 10}
+    //使用Lambda表达式为mResult赋值函数
+    mResult = {it > 10}
+    //此时的mResult为一个方法字面值
+    //testLiteral接受一个参数为Int类型返回值为Boolean的函数体
+    testLiteral(mResult)//调用方式一
+    testLiteral { p -> p > 10 }//调用方式二
 }
 
 /**
@@ -43,20 +67,27 @@ fun List<Int>.sum(callback: (Int) -> Unit): Int {
         result += v
         callback(v)
     }
+    //返回List中所有元素的和
     return result
 }
 
 /**
  * 函数作为返回值
  * 函数功能：对集合元素进行求和的高阶函数，并且返回一个声明为 (scale : Int) -> Float的函数
+ * 函数解析：
+ * 1.首先对Lsit<Int>这种类型进行方法扩展 扩展一个toIntSum方法出来
+ * 2.设置toIntSum方法的返回值为一个方法（函数）
+ * 3.toIntSum返回的方法 接受一个Int类型的参数 并且他的返回值是一个Float类型
  */
 fun List<Int>.toIntSum(): (scale: Int) -> Float {
     //第一层函数
     return fun(scale): Float {
         var reasult = 0f
+        //这里this就代表List<Int>本身
         for (v in this) {
             reasult += v * scale
         }
+        //返回List中所有元素*2的和
         return reasult
     }
 }
@@ -104,4 +135,14 @@ fun literal() {
     temp = { num -> (num > 10) }
     val result = temp(12)
     SLog.print("方法字面值 val result = temp(12) result = $result")
+}
+
+/**
+ * 自己练习方法字面值
+ * 参数P 为一个函数 这个函数接受一个Int类型的参数 会返回一个Boolean类型的参数
+ * 参数P这个函数封装的功能为：判断传入的Int类型的参数是否大于10 返回结果（true或者false）
+ */
+fun testLiteral(p: (Int) -> Boolean) {
+    val invoke = p.invoke(11)
+    SLog.print("自己练习方法字面值 result = $invoke")
 }
